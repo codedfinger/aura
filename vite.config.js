abc7657c4af4903f8658d82b2cfbc0a33885ca5e
codedfingers <alexyikeh@gmail.com>
2024-09-03 23:53:08 +0100

    updated vite config

--- vite.config.js
+++ vite.config.js
@@ -9,7 +9,7 @@ export default defineConfig(({ command, mode }) => {
     const env = loadEnv(mode, process.cwd());
 
     return {
-        base: 'https://codedfinger.github.io/aura/',
+        base: '/aura/',
         plugins: [
             react(),
             createHtmlPlugin({
